84cf1d0c6c315b597e0e07a1dfb09e0136066bad
bm51s <bm51s@ukr.net>
2020-11-19 03:39:49 +0200

    test10

--- src/WeatherWidget/WeatherWidget.js
+++ src/WeatherWidget/WeatherWidget.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import WidgetLayout from "./WidgetLayout";
-
 const url =
   "https://api.openweathermap.org/data/2.5/weather?q=Kyiv&units=metric&appid=5c84bd6294b249fa97ae3ca1c3ca0c4c";
 
@@ -9,12 +7,22 @@ export default class WeatherWidget extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      response: []
+      response: [],
+      weather: "",
+      temp: "",
+      temp_min: "",
+      temp_max: "",
+      pressure: "",
+      humidity: "",
+      wind: "",
+      wind_deg: "",
+      clouds: "",
+      sunrise: "",
+      sunset: ""
     };
-    this.getData();
   }
 
-  getData() {
+  componentDidMount() {
     fetch(url)
       .then((res) => {
         console.log("res " + res);
@@ -22,21 +30,42 @@ export default class WeatherWidget extends React.Component {
       })
       .then((data) => {
         console.log("data " + data);
-        this.setState({ response: data });
-        console.log("this.state.response " + this.state.response);
+        this.setState({
+          response: data.name,
+          weather: data.weather[0].description,
+          temp: data.main.temp,
+          temp_min: data.main.temp_min,
+          temp_max: data.main.temp_max,
+          pressure: data.main.pressure,
+          humidity: data.main.humidity,
+          wind: data.wind.speed,
+          wind_deg: data.wind.deg,
+          clouds: data.clouds.all,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset
+        });
       })
       .catch(() => {
         console.log("error");
       });
   }
 
-  // componentDidMount() {}
-
   render() {
-    console.log("resp render: " + this.state.response);
+    console.log("resp1: " + this.state.response);
+    // let { info } = this.state.response;
     return (
       <>
-        <WidgetLayout info={this.state.response} />
+        <div>city: {this.state.response}</div>
+        <div>weather: {this.state.weather}</div>
+        <div>temp: {this.state.temp}</div>
+        <div>temp_min: {this.state.temp_min}</div>
+        <div>temp_max: {this.state.temp_max}</div>
+        <div>pressure: {this.state.pressure}</div>
+        <div>wind: {this.state.wind}</div>
+        <div>wind_deg: {this.state.wind_deg}</div>
+        <div>clouds: {this.state.clouds}</div>
+        <div>sunrise: {this.state.sunrise}</div>
+        <div>sunset: {this.state.sunset}</div>
       </>
     );
   }
